45d27e95349facdf14ccac1845ca9bb26443cb76
vdtkk <vdtkk@hotmail.com>
2022-09-27 00:45:26 +0300

    denme

---
+++ Work-1/src/work/Main.java
@@ -0,0 +1,27 @@
+package work;
+
+public class Main {
+
+	public static void main(String[] args) {
+	 
+
+
+		
+		int sayi=24;
+		
+		if(sayi<20){
+			
+			System.out.println("sayı 20'den küçüktür.");
+			
+				}
+		else if(sayi==20){
+			System.out.println("eşittir");}
+
+		else{
+			
+			System.out.println("küçük değildir.");
+		}
+		
+	}
+
+}
