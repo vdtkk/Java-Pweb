023e487646c21c17dce40ee9f2ac81716d312a0c
vdtkk <vdtkk@hotmail.com>
2022-10-16 13:31:59 +0300

    oop

---
+++ OOP1/src/oop1/Main.java
@@ -0,0 +1,29 @@
+package oop1;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	
+		
+		Product P1 = new Product();
+		
+				//set value
+		P1.name="Delong Kahve Makinesi";
+		P1.unitPrice=7500;
+		P1.unitInStock=100;
+		P1.discount=7;
+		P1.imageUrl="deneme.jpg";
+		
+		
+				//get - okumak
+		
+		System.out.println(P1.name);
+		
+		
+		
+		
+
+	}
+
+}
---
+++ OOP1/src/oop1/Product.java
@@ -0,0 +1,16 @@
+package oop1;
+
+public  class Product {
+	
+	
+	//camelCase yazılır.
+	String name;
+	double unitPrice; //fiyat
+	double discount; //indirim
+	String imageUrl;
+	int unitInStock;
+	
+	
+	
+	
+}
